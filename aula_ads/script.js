2975008c712c992cad1cba4207d5c2349d771547
kamilly vitoria <kamilly30.vitoriasilva@gmail.com>
2026-08-26 10:15:58 -0300

    priemira alteração

---
+++ aula_ads/script.js
@@ -0,0 +1,25 @@
+let num1 = Number(prompt("Digite o primeiro número:"));
+let operacao = prompt("Digite a operação (+, -, *, /):");
+let num2 = Number(prompt("Digite o segundo número:"));
+
+let resultado;
+
+if (operacao === "+") {
+    resultado = num1 + num2;
+} else if (operacao === "-") {
+    resultado = num1 - num2;
+} else if (operacao === "*") {
+    resultado = num1 * num2;
+} else if (operacao === "/") {
+    if (num2 !== 0) {
+        resultado = num1 / num2;
+    } else {
+        alert("Não é possível dividir por zero!");
+    }
+} else {
+    alert("Operação inválida!");
+}
+
+if (resultado !== undefined) {
+    alert("Resultado: " + resultado);
+}
